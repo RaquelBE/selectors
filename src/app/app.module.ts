6aa70a032e43e6221cef9ce5e3df68b7bf4079a9
Raquel Blazquez <raquel.blazquez@bosonit.com>
2023-09-20 11:36:09 +0200

    creación countries-routing.module y countris.module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,16 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { CountriesModule } from './countries/countries.module';
+import { AppRoutingModule } from './app-routing.module';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
