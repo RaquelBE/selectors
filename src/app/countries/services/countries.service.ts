0ae8fc01321717346be401b189512806bc8b4fd9
Raquel Blazquez <raquel.blazquez@bosonit.com>
2023-09-20 13:30:20 +0200

    obtener países por región

--- src/app/countries/services/countries.service.ts
+++ src/app/countries/services/countries.service.ts
@@ -1,10 +1,13 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 import { Region, SmallCountry } from '../interfaces/country.interfaces';
+import { Observable, of, tap } from 'rxjs';
 
 @Injectable({
   providedIn: 'root',
 })
 export class CountriesService {
+  private baseUrl: string = 'https://restcountries.com/v3.1';
   private _regions: Region[] = [
     Region.Africa,
     Region.Americas,
@@ -13,13 +16,20 @@ export class CountriesService {
     Region.Oceania,
   ];
 
-  constructor() {}
+  constructor(private http: HttpClient) {}
 
   get regions(): Region[] {
     return [...this._regions];
   }
 
-  getCountriesByRegion(region: Region): SmallCountry[] {
-    return [];
+  getCountriesByRegion(region: Region): Observable<SmallCountry[]> {
+    if (!region) return of([]);
+
+    const url: string = `${this.baseUrl}/region/${region}?fields=cca3,name,borders`;
+
+    return this.http.get<SmallCountry[]>(url)
+    .pipe(
+      tap( response => console.log({response}))
+    )
   }
 }
