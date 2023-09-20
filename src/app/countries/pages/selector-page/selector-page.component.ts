417a41ad74457550447e007c55f699c2b97981d1
Raquel Blazquez <raquel.blazquez@bosonit.com>
2023-09-20 13:38:21 +0200

    convertir los países y mostrarlos en pantalla

--- src/app/countries/pages/selector-page/selector-page.component.ts
+++ src/app/countries/pages/selector-page/selector-page.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { CountriesService } from '../../services/countries.service';
-import { Region } from '../../interfaces/country.interfaces';
+import { Region, SmallCountry } from '../../interfaces/country.interfaces';
 import { switchMap } from 'rxjs';
 
 @Component({
@@ -10,6 +10,10 @@ import { switchMap } from 'rxjs';
   styles: [],
 })
 export class SelectorPageComponent implements OnInit {
+
+  public countriesByRegion: SmallCountry[] = [];
+
+
   public myForm: FormGroup = this.fb.group({
     region: ['', Validators.required],
     country: ['', Validators.required],
@@ -38,8 +42,8 @@ export class SelectorPageComponent implements OnInit {
           this.countriesService.getCountriesByRegion(region)
         )
       )
-      .subscribe((region) => {
-        console.log({ region });
+      .subscribe((countries) => {
+        this.countriesByRegion = countries; //esto hace que se puedan mostrar en el HTML
       });
   }
 }
